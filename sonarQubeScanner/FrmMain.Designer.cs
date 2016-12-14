a7249b176debdf09f097237699e524856179b916
ChenWes <chenxuhua0530@163.com>
2016-12-14 14:58:35 +0800

    project init

---
+++ sonarQubeScanner/FrmMain.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace sonarQubeScanner
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab_Main = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lab_ProjectFolder = new System.Windows.Forms.Label();
+            this.txt_ScannerLog = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txt_SourceEncoding = new System.Windows.Forms.TextBox();
+            this.txt_Language = new System.Windows.Forms.TextBox();
+            this.txt_ProjectVersion = new System.Windows.Forms.TextBox();
+            this.txt_Source = new System.Windows.Forms.TextBox();
+            this.txt_ProjectName = new System.Windows.Forms.TextBox();
+            this.lab_SourceEncoding = new System.Windows.Forms.Label();
+            this.txt_ProjectKey = new System.Windows.Forms.TextBox();
+            this.lab_Language = new System.Windows.Forms.Label();
+            this.lab_ProjectVersion = new System.Windows.Forms.Label();
+            this.lab_source = new System.Windows.Forms.Label();
+            this.lab_ProjectName = new System.Windows.Forms.Label();
+            this.lab_ProjectKey = new System.Windows.Forms.Label();
+            this.cmd_OpenProjectFolder = new System.Windows.Forms.Button();
+            this.txt_ProjectFolder = new System.Windows.Forms.TextBox();
+            this.btn_runner = new System.Windows.Forms.Button();
+            this.folderBrowserDialog_ProjectFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.tab_Main.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab_Main
+            // 
+            this.tab_Main.Controls.Add(this.tabPage2);
+            this.tab_Main.Location = new System.Drawing.Point(12, 12);
+            this.tab_Main.Name = "tab_Main";
+            this.tab_Main.SelectedIndex = 0;
+            this.tab_Main.Size = new System.Drawing.Size(765, 563);
+            this.tab_Main.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.lab_ProjectFolder);
+            this.tabPage2.Controls.Add(this.txt_ScannerLog);
+            this.tabPage2.Controls.Add(this.panel1);
+            this.tabPage2.Controls.Add(this.cmd_OpenProjectFolder);
+            this.tabPage2.Controls.Add(this.txt_ProjectFolder);
+            this.tabPage2.Controls.Add(this.btn_runner);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(757, 537);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "sonarScanner";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lab_ProjectFolder
+            // 
+            this.lab_ProjectFolder.AutoSize = true;
+            this.lab_ProjectFolder.Location = new System.Drawing.Point(10, 11);
+            this.lab_ProjectFolder.Name = "lab_ProjectFolder";
+            this.lab_ProjectFolder.Size = new System.Drawing.Size(89, 12);
+            this.lab_ProjectFolder.TabIndex = 7;
+            this.lab_ProjectFolder.Text = "Project Folder";
+            // 
+            // txt_ScannerLog
+            // 
+            this.txt_ScannerLog.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_ScannerLog.Location = new System.Drawing.Point(6, 144);
+            this.txt_ScannerLog.Name = "txt_ScannerLog";
+            this.txt_ScannerLog.Size = new System.Drawing.Size(740, 358);
+            this.txt_ScannerLog.TabIndex = 6;
+            this.txt_ScannerLog.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txt_SourceEncoding);
+            this.panel1.Controls.Add(this.txt_Language);
+            this.panel1.Controls.Add(this.txt_ProjectVersion);
+            this.panel1.Controls.Add(this.txt_Source);
+            this.panel1.Controls.Add(this.txt_ProjectName);
+            this.panel1.Controls.Add(this.lab_SourceEncoding);
+            this.panel1.Controls.Add(this.txt_ProjectKey);
+            this.panel1.Controls.Add(this.lab_Language);
+            this.panel1.Controls.Add(this.lab_ProjectVersion);
+            this.panel1.Controls.Add(this.lab_source);
+            this.panel1.Controls.Add(this.lab_ProjectName);
+            this.panel1.Controls.Add(this.lab_ProjectKey);
+            this.panel1.Location = new System.Drawing.Point(6, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(745, 92);
+            this.panel1.TabIndex = 3;
+            // 
+            // txt_SourceEncoding
+            // 
+            this.txt_SourceEncoding.Location = new System.Drawing.Point(477, 64);
+            this.txt_SourceEncoding.Name = "txt_SourceEncoding";
+            this.txt_SourceEncoding.Size = new System.Drawing.Size(263, 21);
+            this.txt_SourceEncoding.TabIndex = 9;
+            this.txt_SourceEncoding.Text = "UTF-8";
+            // 
+            // txt_Language
+            // 
+            this.txt_Language.Location = new System.Drawing.Point(477, 35);
+            this.txt_Language.Name = "txt_Language";
+            this.txt_Language.Size = new System.Drawing.Size(263, 21);
+            this.txt_Language.TabIndex = 9;
+            this.txt_Language.Text = "cs";
+            // 
+            // txt_ProjectVersion
+            // 
+            this.txt_ProjectVersion.Location = new System.Drawing.Point(99, 64);
+            this.txt_ProjectVersion.Name = "txt_ProjectVersion";
+            this.txt_ProjectVersion.Size = new System.Drawing.Size(263, 21);
+            this.txt_ProjectVersion.TabIndex = 9;
+            this.txt_ProjectVersion.Text = "1.0";
+            // 
+            // txt_Source
+            // 
+            this.txt_Source.Location = new System.Drawing.Point(477, 7);
+            this.txt_Source.Name = "txt_Source";
+            this.txt_Source.Size = new System.Drawing.Size(263, 21);
+            this.txt_Source.TabIndex = 9;
+            this.txt_Source.Text = ".";
+            // 
+            // txt_ProjectName
+            // 
+            this.txt_ProjectName.Location = new System.Drawing.Point(99, 35);
+            this.txt_ProjectName.Name = "txt_ProjectName";
+            this.txt_ProjectName.Size = new System.Drawing.Size(263, 21);
+            this.txt_ProjectName.TabIndex = 9;
+            this.txt_ProjectName.Text = "sonarScannerForEsquel";
+            // 
+            // lab_SourceEncoding
+            // 
+            this.lab_SourceEncoding.AutoSize = true;
+            this.lab_SourceEncoding.Location = new System.Drawing.Point(382, 67);
+            this.lab_SourceEncoding.Name = "lab_SourceEncoding";
+            this.lab_SourceEncoding.Size = new System.Drawing.Size(95, 12);
+            this.lab_SourceEncoding.TabIndex = 8;
+            this.lab_SourceEncoding.Text = "Source Encoding";
+            // 
+            // txt_ProjectKey
+            // 
+            this.txt_ProjectKey.Location = new System.Drawing.Point(99, 7);
+            this.txt_ProjectKey.Name = "txt_ProjectKey";
+            this.txt_ProjectKey.Size = new System.Drawing.Size(263, 21);
+            this.txt_ProjectKey.TabIndex = 9;
+            this.txt_ProjectKey.Text = "sonarScannerForEsquel";
+            // 
+            // lab_Language
+            // 
+            this.lab_Language.AutoSize = true;
+            this.lab_Language.Location = new System.Drawing.Point(382, 38);
+            this.lab_Language.Name = "lab_Language";
+            this.lab_Language.Size = new System.Drawing.Size(53, 12);
+            this.lab_Language.TabIndex = 8;
+            this.lab_Language.Text = "Language";
+            // 
+            // lab_ProjectVersion
+            // 
+            this.lab_ProjectVersion.AutoSize = true;
+            this.lab_ProjectVersion.Location = new System.Drawing.Point(4, 67);
+            this.lab_ProjectVersion.Name = "lab_ProjectVersion";
+            this.lab_ProjectVersion.Size = new System.Drawing.Size(95, 12);
+            this.lab_ProjectVersion.TabIndex = 8;
+            this.lab_ProjectVersion.Text = "Project Version";
+            // 
+            // lab_source
+            // 
+            this.lab_source.AutoSize = true;
+            this.lab_source.Location = new System.Drawing.Point(382, 10);
+            this.lab_source.Name = "lab_source";
+            this.lab_source.Size = new System.Drawing.Size(41, 12);
+            this.lab_source.TabIndex = 8;
+            this.lab_source.Text = "Source";
+            // 
+            // lab_ProjectName
+            // 
+            this.lab_ProjectName.AutoSize = true;
+            this.lab_ProjectName.Location = new System.Drawing.Point(4, 38);
+            this.lab_ProjectName.Name = "lab_ProjectName";
+            this.lab_ProjectName.Size = new System.Drawing.Size(77, 12);
+            this.lab_ProjectName.TabIndex = 8;
+            this.lab_ProjectName.Text = "Project Name";
+            // 
+            // lab_ProjectKey
+            // 
+            this.lab_ProjectKey.AutoSize = true;
+            this.lab_ProjectKey.Location = new System.Drawing.Point(4, 10);
+            this.lab_ProjectKey.Name = "lab_ProjectKey";
+            this.lab_ProjectKey.Size = new System.Drawing.Size(71, 12);
+            this.lab_ProjectKey.TabIndex = 8;
+            this.lab_ProjectKey.Text = "Project Key";
+            // 
+            // cmd_OpenProjectFolder
+            // 
+            this.cmd_OpenProjectFolder.Location = new System.Drawing.Point(637, 8);
+            this.cmd_OpenProjectFolder.Name = "cmd_OpenProjectFolder";
+            this.cmd_OpenProjectFolder.Size = new System.Drawing.Size(109, 23);
+            this.cmd_OpenProjectFolder.TabIndex = 2;
+            this.cmd_OpenProjectFolder.Text = "Project Folder";
+            this.cmd_OpenProjectFolder.UseVisualStyleBackColor = true;
+            this.cmd_OpenProjectFolder.Click += new System.EventHandler(this.cmd_OpenProjectFolder_Click);
+            // 
+            // txt_ProjectFolder
+            // 
+            this.txt_ProjectFolder.Enabled = false;
+            this.txt_ProjectFolder.Location = new System.Drawing.Point(105, 8);
+            this.txt_ProjectFolder.Name = "txt_ProjectFolder";
+            this.txt_ProjectFolder.Size = new System.Drawing.Size(526, 21);
+            this.txt_ProjectFolder.TabIndex = 1;
+            this.txt_ProjectFolder.Text = "F:\\test\\sonarQubeScanner\\sonarQubeScanner";
+            // 
+            // btn_runner
+            // 
+            this.btn_runner.Location = new System.Drawing.Point(645, 508);
+            this.btn_runner.Name = "btn_runner";
+            this.btn_runner.Size = new System.Drawing.Size(106, 23);
+            this.btn_runner.TabIndex = 0;
+            this.btn_runner.Text = "Run";
+            this.btn_runner.UseVisualStyleBackColor = true;
+            this.btn_runner.Click += new System.EventHandler(this.btn_runner_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 587);
+            this.Controls.Add(this.tab_Main);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "sonarQube Scanner For .Net Tools";
+            this.tab_Main.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tab_Main;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btn_runner;
+        private System.Windows.Forms.Button cmd_OpenProjectFolder;
+        private System.Windows.Forms.TextBox txt_ProjectFolder;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox txt_ScannerLog;
+        private System.Windows.Forms.Label lab_ProjectFolder;
+        private System.Windows.Forms.TextBox txt_ProjectVersion;
+        private System.Windows.Forms.TextBox txt_ProjectName;
+        private System.Windows.Forms.TextBox txt_ProjectKey;
+        private System.Windows.Forms.Label lab_ProjectVersion;
+        private System.Windows.Forms.Label lab_ProjectName;
+        private System.Windows.Forms.Label lab_ProjectKey;
+        private System.Windows.Forms.TextBox txt_SourceEncoding;
+        private System.Windows.Forms.TextBox txt_Language;
+        private System.Windows.Forms.TextBox txt_Source;
+        private System.Windows.Forms.Label lab_SourceEncoding;
+        private System.Windows.Forms.Label lab_Language;
+        private System.Windows.Forms.Label lab_source;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog_ProjectFolder;
+
+    }
+}
+
---
+++ sonarQubeScanner/frmMain.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System.Diagnostics;
+using System.IO;
+
+namespace sonarQubeScanner
+{
+    // 1.定义委托
+    public delegate void DelReadStdOutput(string result);
+    public delegate void DelReadErrOutput(string result);
+    public delegate void DelRunCompleted(string completeMessage);
+
+    public partial class FrmMain : Form
+    {
+        // 2.定义委托事件  
+        public event DelReadStdOutput ReadStdOutput;
+        public event DelReadErrOutput ReadErrOutput;
+        public event DelRunCompleted RunCompleted;  
+
+        public FrmMain()
+        {
+            InitializeComponent();            
+
+            Init();
+        }
+
+        private void Init()
+        {
+            //3.将相应函数注册到委托事件中  
+            ReadStdOutput += new DelReadStdOutput(ReadStdOutputAction);
+            ReadErrOutput += new DelReadErrOutput(ReadErrOutputAction);
+            RunCompleted += new DelRunCompleted(runner_complete);  
+        }
+
+        private void RealAction(string StartFileName, string StartFileArg)
+        {
+            Process CmdProcess = new Process();
+            CmdProcess.StartInfo.FileName = StartFileName;      // 命令  
+            //CmdProcess.StartInfo.Arguments = StartFileArg;      // 参数  
+
+            CmdProcess.StartInfo.CreateNoWindow = true;         // 不创建新窗口  
+            CmdProcess.StartInfo.UseShellExecute = false;
+
+            CmdProcess.StartInfo.RedirectStandardInput = true;  // 重定向输入  
+            CmdProcess.StartInfo.RedirectStandardOutput = true; // 重定向标准输出  
+            CmdProcess.StartInfo.RedirectStandardError = true;  // 重定向错误输出  
+            //CmdProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;  
+
+
+            CmdProcess.OutputDataReceived += new DataReceivedEventHandler(p_OutputDataReceived);//正常输出信息
+            CmdProcess.ErrorDataReceived += new DataReceivedEventHandler(p_ErrorDataReceived);//输出错误信息
+            CmdProcess.Exited += new EventHandler(CmdProcess_Exited);   // 注册进程结束事件  
+            CmdProcess.EnableRaisingEvents = true;                      // 启用Exited事件  
+
+            CmdProcess.Start();
+
+            //add command
+            CmdProcess.StandardInput.WriteLine(StartFileArg);
+            CmdProcess.StandardInput.AutoFlush = true;
+
+
+            CmdProcess.BeginOutputReadLine();
+            CmdProcess.BeginErrorReadLine();
+
+            // 如果打开注释，则以同步方式执行命令，此例子中用Exited事件异步执行。  
+            // CmdProcess.WaitForExit();
+        }
+
+        private void p_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data != null)
+            {
+                // 4. 异步调用，需要invoke  
+                this.Invoke(ReadStdOutput, new object[] { e.Data });
+            }
+        }
+
+        private void p_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data != null)
+            {
+                this.Invoke(ReadErrOutput, new object[] { e.Data });
+            }
+        }
+
+        private void CmdProcess_Exited(object sender, EventArgs e)
+        {
+            // 执行结束后触发              
+            this.Invoke(RunCompleted, new object[] { "sonarQube Scanner Run Completed !" });
+        }  
+
+        private void ReadStdOutputAction(string result)
+        {
+            this.txt_ScannerLog.SelectionColor = Color.Black;
+            this.txt_ScannerLog.AppendText(result + "\r\n");
+            this.txt_ScannerLog.ScrollToCaret();
+        }
+
+        private void ReadErrOutputAction(string result)
+        {
+            this.txt_ScannerLog.SelectionColor = Color.Red;
+            this.txt_ScannerLog.AppendText(result + "\r\n");
+            this.txt_ScannerLog.ScrollToCaret();
+        }
+
+        private void runner_complete(string completeMessage)
+        {
+            btn_runner.Enabled = true;
+            this.txt_ScannerLog.SelectionColor = Color.Green;
+            this.txt_ScannerLog.AppendText(completeMessage);
+
+            SettingButtons(false);
+        }
+
+
+        public void SettingButtons(bool pi_blnScanFlag)
+        {
+            if(pi_blnScanFlag)txt_ScannerLog.Text = "";
+            
+            btn_runner.Enabled = !pi_blnScanFlag;
+
+            txt_ProjectKey.Enabled = !pi_blnScanFlag;
+            txt_ProjectName.Enabled = !pi_blnScanFlag;
+            txt_ProjectVersion.Enabled = !pi_blnScanFlag;
+            txt_Source.Enabled = !pi_blnScanFlag;
+            txt_SourceEncoding.Enabled = !pi_blnScanFlag;
+            txt_Language.Enabled = !pi_blnScanFlag;
+        }
+
+        public void ShowError(string pi_message)
+        {
+            MessageBox.Show(pi_message);
+        }
+
+        public bool check_projectParameter()
+        {
+            bool l_returnFlag = true;
+
+            if(string.IsNullOrEmpty(txt_ProjectFolder.Text.Trim()))
+            {
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txt_ProjectKey.Text.Trim()))
+            {
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txt_ProjectName.Text.Trim()))
+            {
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txt_ProjectVersion.Text.Trim()))
+            {
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txt_Language.Text.Trim()))
+            {
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txt_Source.Text.Trim()))
+            {
+                return false;
+            }
+
+            return l_returnFlag;
+        }
+
+        private void cmd_OpenProjectFolder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog_ProjectFolder.RootFolder = Environment.SpecialFolder.Desktop;
+            if(folderBrowserDialog_ProjectFolder.ShowDialog()==System.Windows.Forms.DialogResult.OK)
+            {
+                txt_ProjectFolder.Text = folderBrowserDialog_ProjectFolder.SelectedPath;
+            }
+        }
+
+        private void btn_runner_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!check_projectParameter()) return;
+
+                SettingButtons(true);
+
+                string pi_path = System.AppDomain.CurrentDomain.BaseDirectory;
+                string l_root = Path.GetPathRoot(txt_ProjectFolder.Text.Trim());
+
+                StringBuilder l_strParameter = new StringBuilder();
+                l_strParameter.Append(string.Format(" -Dsonar.projectKey=\"{0}\"", txt_ProjectKey.Text.Trim()));
+                l_strParameter.Append(string.Format(" -Dsonar.projectName=\"{0}\"", txt_ProjectName.Text.Trim()));
+                l_strParameter.Append(string.Format(" -Dsonar.projectVersion=\"{0}\"", txt_ProjectVersion.Text.Trim()));
+                l_strParameter.Append(string.Format(" -Dsonar.sources=\"{0}\"", txt_Source.Text.Trim()));
+                l_strParameter.Append(string.Format(" -Dsonar.language=\"{0}\" ", txt_Language.Text.Trim()));
+                l_strParameter.Append(string.Format(" -Dsonar.sourceEncoding=\"{0}\"", txt_SourceEncoding.Text.Trim()));
+
+                string cmd = string.Format("{0}&cd {1}&{2}sonar-scanner-2.8\\bin\\sonar-scanner {3}&exit", l_root.Substring(0, l_root.Length - 1), txt_ProjectFolder.Text.Trim(), pi_path,l_strParameter.ToString());
+
+                #region sonar-project.properties
+                //StringBuilder l_file = new StringBuilder();
+
+                //l_file.Append("# Required metadata" + "\r\n");
+                //l_file.Append(string.Format("sonar.projectKey={0}", txt_ProjectKey.Text.Trim()) + "\r\n");
+                //l_file.Append(string.Format("sonar.projectName={0}", txt_ProjectName.Text.Trim()) + "\r\n");
+                //l_file.Append(string.Format("sonar.projectVersion={0}", txt_ProjectVersion.Text.Trim()) + "\r\n");
+                //l_file.Append("# Comma-separated paths to directories with sources (required)" + "\r\n");
+                //l_file.Append(string.Format("sonar.sources={0}", txt_Source.Text.Trim()) + "\r\n");
+                //l_file.Append("#sonar.binaries=bin\\classes" + "\r\n");
+                //l_file.Append("# Language" + "\r\n");
+                //l_file.Append(string.Format("sonar.language={0}", txt_Language.Text.Trim()) + "\r\n");
+                //l_file.Append("# Encoding of the source files" + "\r\n");
+                //l_file.Append(string.Format("sonar.sourceEncoding={0}", txt_SourceEncoding.Text.Trim()) + "\r\n");
+
+                ////write file
+                //if(File.Exists(txt_ProjectFolder.Text.Trim() + "\\sonar-project.properties"))
+                //{
+                //    File.Delete(txt_ProjectFolder.Text.Trim() + "\\sonar-project.properties");
+                //}
+
+                //FileStream fs = new FileStream(txt_ProjectFolder.Text.Trim() + "\\sonar-project.properties", FileMode.CreateNew);
+                //StreamWriter sw = new StreamWriter(fs, Encoding.Default);
+                //sw.Write(l_file.ToString());
+                //sw.Close();
+                //fs.Close();
+
+                #endregion
+
+                RealAction("cmd.exe", cmd);
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex.Message);
+                SettingButtons(false);
+            }
+        }
+
+        private string GetProjectParameter()
+        {
+            StringBuilder l_scannerParameter = new StringBuilder();
+
+
+            return l_scannerParameter.ToString();
+        }
+    }
+}
