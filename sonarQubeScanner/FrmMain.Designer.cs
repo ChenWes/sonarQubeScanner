e8e651635646d61c9cde5ddcf3d60cbd85c14449
ChenWes <chenxuhua0530@163.com>
2017-01-19 16:16:03 +0800

    add github pull request scan function

--- sonarQubeScanner/FrmMain.Designer.cs
+++ sonarQubeScanner/FrmMain.Designer.cs
@@ -48,23 +48,51 @@
             this.cmd_OpenProjectFolder = new System.Windows.Forms.Button();
             this.txt_ProjectFolder = new System.Windows.Forms.TextBox();
             this.btn_runner = new System.Windows.Forms.Button();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btn_unzipAndSettingParameter = new System.Windows.Forms.Button();
+            this.txt_jdbc_password = new System.Windows.Forms.TextBox();
+            this.lab_jdbc_password = new System.Windows.Forms.Label();
+            this.txt_jdbc_username = new System.Windows.Forms.TextBox();
+            this.lab_jdbc_username = new System.Windows.Forms.Label();
+            this.txt_host_url = new System.Windows.Forms.TextBox();
+            this.lab_host_url = new System.Windows.Forms.Label();
+            this.btn_downloadsonarScanner = new System.Windows.Forms.Button();
             this.folderBrowserDialog_ProjectFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.btn_run_preview = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.txt_sonarqube_token = new System.Windows.Forms.TextBox();
+            this.txt_github_token = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txt_github_pullrequestid = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_github_repository = new System.Windows.Forms.TextBox();
             this.tab_Main.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
             // tab_Main
             // 
             this.tab_Main.Controls.Add(this.tabPage2);
+            this.tab_Main.Controls.Add(this.tabPage1);
             this.tab_Main.Location = new System.Drawing.Point(12, 12);
             this.tab_Main.Name = "tab_Main";
             this.tab_Main.SelectedIndex = 0;
-            this.tab_Main.Size = new System.Drawing.Size(765, 563);
+            this.tab_Main.Size = new System.Drawing.Size(765, 613);
             this.tab_Main.TabIndex = 0;
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.panel5);
+            this.tabPage2.Controls.Add(this.btn_run_preview);
             this.tabPage2.Controls.Add(this.lab_ProjectFolder);
             this.tabPage2.Controls.Add(this.txt_ScannerLog);
             this.tabPage2.Controls.Add(this.panel1);
@@ -74,7 +102,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(757, 537);
+            this.tabPage2.Size = new System.Drawing.Size(757, 587);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "sonarScanner";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -91,14 +119,15 @@
             // txt_ScannerLog
             // 
             this.txt_ScannerLog.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_ScannerLog.Location = new System.Drawing.Point(6, 144);
+            this.txt_ScannerLog.Location = new System.Drawing.Point(6, 201);
             this.txt_ScannerLog.Name = "txt_ScannerLog";
-            this.txt_ScannerLog.Size = new System.Drawing.Size(740, 358);
+            this.txt_ScannerLog.Size = new System.Drawing.Size(742, 352);
             this.txt_ScannerLog.TabIndex = 6;
             this.txt_ScannerLog.Text = "";
             // 
             // panel1
             // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.txt_SourceEncoding);
             this.panel1.Controls.Add(this.txt_Language);
             this.panel1.Controls.Add(this.txt_ProjectVersion);
@@ -111,7 +140,7 @@
             this.panel1.Controls.Add(this.lab_source);
             this.panel1.Controls.Add(this.lab_ProjectName);
             this.panel1.Controls.Add(this.lab_ProjectKey);
-            this.panel1.Location = new System.Drawing.Point(6, 46);
+            this.panel1.Location = new System.Drawing.Point(6, 37);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(745, 92);
             this.panel1.TabIndex = 3;
@@ -154,7 +183,7 @@
             this.txt_ProjectName.Name = "txt_ProjectName";
             this.txt_ProjectName.Size = new System.Drawing.Size(263, 21);
             this.txt_ProjectName.TabIndex = 9;
-            this.txt_ProjectName.Text = "sonarScannerForEsquel";
+            this.txt_ProjectName.Text = "sonarScannerProjectForEsquel";
             // 
             // lab_SourceEncoding
             // 
@@ -171,7 +200,7 @@
             this.txt_ProjectKey.Name = "txt_ProjectKey";
             this.txt_ProjectKey.Size = new System.Drawing.Size(263, 21);
             this.txt_ProjectKey.TabIndex = 9;
-            this.txt_ProjectKey.Text = "sonarScannerForEsquel";
+            this.txt_ProjectKey.Text = "sonarScannerProjectForEsquel";
             // 
             // lab_Language
             // 
@@ -222,7 +251,7 @@
             // 
             this.cmd_OpenProjectFolder.Location = new System.Drawing.Point(637, 8);
             this.cmd_OpenProjectFolder.Name = "cmd_OpenProjectFolder";
-            this.cmd_OpenProjectFolder.Size = new System.Drawing.Size(109, 23);
+            this.cmd_OpenProjectFolder.Size = new System.Drawing.Size(114, 23);
             this.cmd_OpenProjectFolder.TabIndex = 2;
             this.cmd_OpenProjectFolder.Text = "Project Folder";
             this.cmd_OpenProjectFolder.UseVisualStyleBackColor = true;
@@ -235,11 +264,10 @@
             this.txt_ProjectFolder.Name = "txt_ProjectFolder";
             this.txt_ProjectFolder.Size = new System.Drawing.Size(526, 21);
             this.txt_ProjectFolder.TabIndex = 1;
-            this.txt_ProjectFolder.Text = "F:\\test\\sonarQubeScanner\\sonarQubeScanner";
             // 
             // btn_runner
             // 
-            this.btn_runner.Location = new System.Drawing.Point(645, 508);
+            this.btn_runner.Location = new System.Drawing.Point(642, 558);
             this.btn_runner.Name = "btn_runner";
             this.btn_runner.Size = new System.Drawing.Size(106, 23);
             this.btn_runner.TabIndex = 0;
@@ -247,20 +275,223 @@
             this.btn_runner.UseVisualStyleBackColor = true;
             this.btn_runner.Click += new System.EventHandler(this.btn_runner_Click);
             // 
-            // frmMain
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.panel2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(757, 537);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "setting";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.btn_downloadsonarScanner);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(748, 180);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.btn_unzipAndSettingParameter);
+            this.panel3.Controls.Add(this.txt_jdbc_password);
+            this.panel3.Controls.Add(this.lab_jdbc_password);
+            this.panel3.Controls.Add(this.txt_jdbc_username);
+            this.panel3.Controls.Add(this.lab_jdbc_username);
+            this.panel3.Controls.Add(this.txt_host_url);
+            this.panel3.Controls.Add(this.lab_host_url);
+            this.panel3.Location = new System.Drawing.Point(12, 65);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(723, 100);
+            this.panel3.TabIndex = 2;
+            // 
+            // btn_unzipAndSettingParameter
+            // 
+            this.btn_unzipAndSettingParameter.Location = new System.Drawing.Point(452, 67);
+            this.btn_unzipAndSettingParameter.Name = "btn_unzipAndSettingParameter";
+            this.btn_unzipAndSettingParameter.Size = new System.Drawing.Size(250, 23);
+            this.btn_unzipAndSettingParameter.TabIndex = 2;
+            this.btn_unzipAndSettingParameter.Text = "unZip File And Setting Parameter";
+            this.btn_unzipAndSettingParameter.UseVisualStyleBackColor = true;
+            this.btn_unzipAndSettingParameter.Click += new System.EventHandler(this.btn_unzipAndSettingParameter_Click);
+            // 
+            // txt_jdbc_password
+            // 
+            this.txt_jdbc_password.Location = new System.Drawing.Point(138, 67);
+            this.txt_jdbc_password.Name = "txt_jdbc_password";
+            this.txt_jdbc_password.Size = new System.Drawing.Size(298, 21);
+            this.txt_jdbc_password.TabIndex = 1;
+            this.txt_jdbc_password.Text = "sonar";
+            // 
+            // lab_jdbc_password
+            // 
+            this.lab_jdbc_password.AutoSize = true;
+            this.lab_jdbc_password.Location = new System.Drawing.Point(12, 70);
+            this.lab_jdbc_password.Name = "lab_jdbc_password";
+            this.lab_jdbc_password.Size = new System.Drawing.Size(125, 12);
+            this.lab_jdbc_password.TabIndex = 0;
+            this.lab_jdbc_password.Text = "sonar.jdbc.password:";
+            // 
+            // txt_jdbc_username
+            // 
+            this.txt_jdbc_username.Location = new System.Drawing.Point(138, 40);
+            this.txt_jdbc_username.Name = "txt_jdbc_username";
+            this.txt_jdbc_username.Size = new System.Drawing.Size(298, 21);
+            this.txt_jdbc_username.TabIndex = 1;
+            this.txt_jdbc_username.Text = "sonar";
+            // 
+            // lab_jdbc_username
+            // 
+            this.lab_jdbc_username.AutoSize = true;
+            this.lab_jdbc_username.Location = new System.Drawing.Point(12, 43);
+            this.lab_jdbc_username.Name = "lab_jdbc_username";
+            this.lab_jdbc_username.Size = new System.Drawing.Size(125, 12);
+            this.lab_jdbc_username.TabIndex = 0;
+            this.lab_jdbc_username.Text = "sonar.jdbc.username:";
+            // 
+            // txt_host_url
+            // 
+            this.txt_host_url.Location = new System.Drawing.Point(138, 13);
+            this.txt_host_url.Name = "txt_host_url";
+            this.txt_host_url.Size = new System.Drawing.Size(298, 21);
+            this.txt_host_url.TabIndex = 1;
+            this.txt_host_url.Text = "http://localhost:9000";
+            // 
+            // lab_host_url
+            // 
+            this.lab_host_url.AutoSize = true;
+            this.lab_host_url.Location = new System.Drawing.Point(12, 16);
+            this.lab_host_url.Name = "lab_host_url";
+            this.lab_host_url.Size = new System.Drawing.Size(95, 12);
+            this.lab_host_url.TabIndex = 0;
+            this.lab_host_url.Text = "sonar.host.url:";
+            // 
+            // btn_downloadsonarScanner
+            // 
+            this.btn_downloadsonarScanner.Location = new System.Drawing.Point(12, 23);
+            this.btn_downloadsonarScanner.Name = "btn_downloadsonarScanner";
+            this.btn_downloadsonarScanner.Size = new System.Drawing.Size(250, 23);
+            this.btn_downloadsonarScanner.TabIndex = 0;
+            this.btn_downloadsonarScanner.Text = "Download sonarQube Scanner";
+            this.btn_downloadsonarScanner.UseVisualStyleBackColor = true;
+            this.btn_downloadsonarScanner.Click += new System.EventHandler(this.btn_downloadsonarScanner_Click);
+            // 
+            // btn_run_preview
+            // 
+            this.btn_run_preview.Location = new System.Drawing.Point(496, 558);
+            this.btn_run_preview.Name = "btn_run_preview";
+            this.btn_run_preview.Size = new System.Drawing.Size(115, 23);
+            this.btn_run_preview.TabIndex = 8;
+            this.btn_run_preview.Text = "github Preview";
+            this.btn_run_preview.UseVisualStyleBackColor = true;
+            this.btn_run_preview.Click += new System.EventHandler(this.btn_run_preview_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Controls.Add(this.txt_github_repository);
+            this.panel5.Controls.Add(this.label8);
+            this.panel5.Controls.Add(this.txt_github_pullrequestid);
+            this.panel5.Controls.Add(this.label7);
+            this.panel5.Controls.Add(this.txt_sonarqube_token);
+            this.panel5.Controls.Add(this.txt_github_token);
+            this.panel5.Controls.Add(this.label10);
+            this.panel5.Controls.Add(this.label12);
+            this.panel5.Location = new System.Drawing.Point(6, 134);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(745, 61);
+            this.panel5.TabIndex = 11;
+            // 
+            // txt_sonarqube_token
+            // 
+            this.txt_sonarqube_token.Location = new System.Drawing.Point(477, 7);
+            this.txt_sonarqube_token.Name = "txt_sonarqube_token";
+            this.txt_sonarqube_token.Size = new System.Drawing.Size(263, 21);
+            this.txt_sonarqube_token.TabIndex = 9;
+            // 
+            // txt_github_token
+            // 
+            this.txt_github_token.Location = new System.Drawing.Point(99, 7);
+            this.txt_github_token.Name = "txt_github_token";
+            this.txt_github_token.Size = new System.Drawing.Size(263, 21);
+            this.txt_github_token.TabIndex = 9;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(382, 10);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(95, 12);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "sonarQube Token";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(4, 10);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(77, 12);
+            this.label12.TabIndex = 8;
+            this.label12.Text = "Github Token";
+            // 
+            // txt_github_pullrequestid
+            // 
+            this.txt_github_pullrequestid.Location = new System.Drawing.Point(99, 33);
+            this.txt_github_pullrequestid.Name = "txt_github_pullrequestid";
+            this.txt_github_pullrequestid.Size = new System.Drawing.Size(263, 21);
+            this.txt_github_pullrequestid.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(4, 36);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 12);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Pull Request ID";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(383, 36);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 12);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Github Repository";
+            // 
+            // txt_github_repository
+            // 
+            this.txt_github_repository.Location = new System.Drawing.Point(478, 33);
+            this.txt_github_repository.Name = "txt_github_repository";
+            this.txt_github_repository.Size = new System.Drawing.Size(263, 21);
+            this.txt_github_repository.TabIndex = 11;
+            // 
+            // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(789, 587);
+            this.ClientSize = new System.Drawing.Size(789, 637);
             this.Controls.Add(this.tab_Main);
             this.MaximizeBox = false;
-            this.Name = "frmMain";
+            this.Name = "FrmMain";
             this.Text = "sonarQube Scanner For .Net Tools";
             this.tab_Main.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -288,6 +519,27 @@
         private System.Windows.Forms.Label lab_Language;
         private System.Windows.Forms.Label lab_source;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog_ProjectFolder;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_downloadsonarScanner;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txt_host_url;
+        private System.Windows.Forms.Label lab_host_url;
+        private System.Windows.Forms.TextBox txt_jdbc_password;
+        private System.Windows.Forms.Label lab_jdbc_password;
+        private System.Windows.Forms.TextBox txt_jdbc_username;
+        private System.Windows.Forms.Label lab_jdbc_username;
+        private System.Windows.Forms.Button btn_unzipAndSettingParameter;
+        private System.Windows.Forms.Button btn_run_preview;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.TextBox txt_sonarqube_token;
+        private System.Windows.Forms.TextBox txt_github_token;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txt_github_pullrequestid;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_github_repository;
+        private System.Windows.Forms.Label label8;
 
     }
 }
--- sonarQubeScanner/frmMain.cs
+++ sonarQubeScanner/frmMain.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 
 using System.Diagnostics;
 using System.IO;
+using System.Net;
 
 namespace sonarQubeScanner
 {
@@ -18,19 +19,28 @@ namespace sonarQubeScanner
     public delegate void DelReadStdOutput(string result);
     public delegate void DelReadErrOutput(string result);
     public delegate void DelRunCompleted(string completeMessage);
+    
 
     public partial class FrmMain : Form
     {
         // 2.定义委托事件  
         public event DelReadStdOutput ReadStdOutput;
         public event DelReadErrOutput ReadErrOutput;
-        public event DelRunCompleted RunCompleted;  
+        public event DelRunCompleted RunCompleted;
+        public string mc_runPath = string.Empty;
+        public string mc_sonarRunnerUrl = "https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-2.8.zip";
+        public string mc_sonarRunnerZipFile = "sonar-scanner-2.8.zip";
+        public string mc_sonarRunnerFolderName = "sonar-scanner-2.8";
 
         public FrmMain()
         {
-            InitializeComponent();            
+            InitializeComponent();
 
             Init();
+
+            mc_runPath = Path.GetDirectoryName(Application.ExecutablePath);
+            CheckZipFileExist();
+            ChecksonarScannerFolder();
         }
 
         private void Init()
@@ -140,6 +150,11 @@ namespace sonarQubeScanner
         {
             MessageBox.Show(pi_message);
         }
+        
+        public void ShowError(Exception ex)
+        {
+            MessageBox.Show(ex.StackTrace, "sonarQube Scanner Tool Error Message");
+        }
 
         public bool check_projectParameter()
         {
@@ -178,6 +193,31 @@ namespace sonarQubeScanner
             return l_returnFlag;
         }
 
+        public void CheckZipFileExist()
+        {
+            string l_zipFileFullPath = mc_runPath + "\\"+mc_sonarRunnerZipFile;
+            if (File.Exists(l_zipFileFullPath))
+            {
+                btn_downloadsonarScanner.Enabled = false;
+            }
+        }
+
+        public void ChecksonarScannerFolder()
+        {
+            string l_sonarScannPath = mc_runPath + "\\" + mc_sonarRunnerFolderName;
+            if(!Directory.Exists(l_sonarScannPath))
+            {
+                btn_runner.Enabled = false;
+                btn_unzipAndSettingParameter.Enabled = false;
+            }
+            else
+            {
+                btn_runner.Enabled = true;
+                btn_unzipAndSettingParameter.Enabled = true;
+            }
+        }
+
+        //-------------------------------------------------------------------------------------------------
         private void cmd_OpenProjectFolder_Click(object sender, EventArgs e)
         {
             try
@@ -211,6 +251,7 @@ namespace sonarQubeScanner
                 string l_root = Path.GetPathRoot(txt_ProjectFolder.Text.Trim());
 
                 StringBuilder l_strParameter = new StringBuilder();
+                l_strParameter.Append(" -X");
                 l_strParameter.Append(string.Format(" -Dsonar.projectKey=\"{0}\"", txt_ProjectKey.Text.Trim()));
                 l_strParameter.Append(string.Format(" -Dsonar.projectName=\"{0}\"", txt_ProjectName.Text.Trim()));
                 l_strParameter.Append(string.Format(" -Dsonar.projectVersion=\"{0}\"", txt_ProjectVersion.Text.Trim()));
@@ -257,5 +298,92 @@ namespace sonarQubeScanner
                 SettingButtons(false);
             }
         }
-    }
+
+        private void btn_run_preview_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!check_projectParameter()) return;
+                if (string.IsNullOrEmpty(txt_github_token.Text.Trim())) return;
+                if (string.IsNullOrEmpty(txt_sonarqube_token.Text.Trim())) return;
+                if (string.IsNullOrEmpty(txt_github_pullrequestid.Text.Trim())) return;
+                if (string.IsNullOrEmpty(txt_github_repository.Text.Trim())) return;
+
+                SettingButtons(true);
+
+                string pi_path = System.AppDomain.CurrentDomain.BaseDirectory;
+                string l_root = Path.GetPathRoot(txt_ProjectFolder.Text.Trim());
+
+                StringBuilder l_strParameter = new StringBuilder();
+
+                l_strParameter.Append(" -X");
+                l_strParameter.Append(" -Dsonar.analysis.mode=preview");
+                l_strParameter.Append(string.Format(" -Dsonar.github.pullRequest={0}", txt_github_pullrequestid.Text.Trim()));
+                l_strParameter.Append(string.Format(" -Dsonar.github.repository={0}", txt_github_repository.Text.Trim()));
+                l_strParameter.Append(string.Format(" -Dsonar.github.oauth={0}", txt_github_token.Text.Trim()));
+                l_strParameter.Append(" -Dsonar.host.url=http://localhost:9000/");
+                l_strParameter.Append(string.Format(" -Dsonar.login={0}", txt_sonarqube_token.Text.Trim()));
+                l_strParameter.Append(string.Format(" -Dsonar.projectKey=\"{0}\"", txt_ProjectKey.Text.Trim()));
+                l_strParameter.Append(string.Format(" -Dsonar.sources=\"{0}\"", txt_Source.Text.Trim()));
+
+                string cmd = string.Format("{0}&cd {1}&{2}sonar-scanner-2.8\\bin\\sonar-scanner {3}&exit", l_root.Substring(0, l_root.Length - 1), txt_ProjectFolder.Text.Trim(), pi_path, l_strParameter.ToString());
+
+
+                RealAction("cmd.exe", cmd);
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex.Message);
+                SettingButtons(false);
+            }
+        }
+        //-------------------------------------------------------------------------------------------------        
+
+        private void btn_downloadsonarScanner_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                string l_zipFileFullPath = mc_runPath + "\\" + mc_sonarRunnerZipFile;
+                if (File.Exists(l_zipFileFullPath))
+                {
+                    File.Delete(l_zipFileFullPath);
+                }
+
+                client.DownloadFile(mc_sonarRunnerUrl, l_zipFileFullPath);
+                CheckZipFileExist();
+
+                btn_unzipAndSettingParameter.Enabled=true;
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex);
+            }
+        }
+
+        private void btn_unzipAndSettingParameter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string l_zipFilePath=mc_runPath+"\\"+mc_sonarRunnerZipFile;
+                string l_sonarScannerFolder=mc_runPath+"\\"+mc_sonarRunnerFolderName;
+                if(!Directory.Exists(l_sonarScannerFolder))
+                {
+                    Directory.CreateDirectory(l_sonarScannerFolder);
+                }
+
+                ZipHelper.UnZip(l_zipFilePath, mc_runPath, "", true);
+
+                ChecksonarScannerFolder();
+
+                sonarScannParameterHelper l_class=new sonarScannParameterHelper(mc_runPath+"\\"+"sonar-scanner-2.8\\conf\\sonar-scanner.properties");
+                string l_getvalue=l_class.GetSonarScannerValue("sonar.jdbc.username");
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex);
+            }
+        }
+
+    }    
 }
